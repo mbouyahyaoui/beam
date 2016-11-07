5f230992f6772bae050ad17c8b8592dbc0cc85bb
echauchot <echauchot@gmail.com>
2016-11-07 10:28:30 +0100

    debug script

--- sdks/java/io/elasticsearch/src/test/contrib/create_elk_container.sh
+++ sdks/java/io/elasticsearch/src/test/contrib/create_elk_container.sh
@@ -2,7 +2,7 @@
 docker create -p 5601:5601 -p 9200:9200 -p 5044:5044 -p 5000:5000 -p 9300:9300 -it -v $(pwd):/home/$USER/ --name elk-2.4  sebp/elk:es240_l240_k460
 docker create -p 5601:5601 -p 9200:9200 -p 5044:5044 -p 5000:5000 -p 9300:9300 -it -v $(pwd):/home/$USER/ --name elk-5.0  sebp/elk:es500_l500_k500
 echo "increasing virtual memory for ES 5.0 ..."
-sysctl -w vm.max_map_count=262144
+sudo sysctl -w vm.max_map_count=262144
 cp /etc/sysctl.conf /tmp/sysctl.conf
 echo "vm.max_map_count=262144" >> /tmp/sysctl.conf
 sudo cp /tmp/sysctl.conf /etc/sysctl.conf
