cf91aa11879f732114973ed8925ad922762f0950
echauchot <echauchot@gmail.com>
2016-11-07 09:53:51 +0100

    added elk 5.0 container

--- sdks/java/io/elasticsearch/src/test/contrib/create_elk_container.sh
+++ sdks/java/io/elasticsearch/src/test/contrib/create_elk_container.sh
@@ -1,2 +1,3 @@
 #! /bin/sh
-docker create -p 5601:5601 -p 9200:9200 -p 5044:5044 -p 5000:5000 -p 9300:9300 -it -v $(pwd):/home/$USER/ --name elk  sebp/elk
+docker create -p 5601:5601 -p 9200:9200 -p 5044:5044 -p 5000:5000 -p 9300:9300 -it -v $(pwd):/home/$USER/ --name elk-2.4  sebp/elk:es240_l240_k460
+docker create -p 5601:5601 -p 9200:9200 -p 5044:5044 -p 5000:5000 -p 9300:9300 -it -v $(pwd):/home/$USER/ --name elk-5.0  sebp/elk:es500_l500_k500
